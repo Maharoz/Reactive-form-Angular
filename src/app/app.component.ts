14e2f8fb584857a98e527718ad4bbdeb5ce4a8ec
Maharoz <mahruj66@gmail.com>
2018-12-29 19:09:00 +0600

    Reactive setting value and patching value implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,10 +28,27 @@ export class AppComponent implements OnInit {
     this.signupForm.statusChanges.subscribe(
       (value) => console.log(value)
     );
+
+    this.signupForm.setValue({
+       'userData' : {
+         'username' : 'Max',
+         'email' : 'max@test.com'
+       },
+       'gender' : 'male',
+       'hobbies' : []
+    });
+
+    this.signupForm.patchValue({
+      'userData' : {
+        'username' : 'Anna',
+      }
+ 
+   });
   }
 
   onSubmit(){
     console.log(this.signupForm);
+    this.signupForm.reset();
   }
   onAddHobby(){
     const control = new FormControl(null,Validators.required);
